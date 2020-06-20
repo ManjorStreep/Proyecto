9131dbb5fc797b3f2826c6cdb10f3312ee67c29c
ManjorStreep <ManjorStreep@gmail.com>
2020-06-20 13:21:53 -0400

    Creando base de datos

--- FARMATICS/forms/frmCaja.vb
+++ FARMATICS/forms/frmCaja.vb
@@ -3,4 +3,13 @@
     Private Sub btn_client_Click(sender As Object, e As EventArgs) Handles btn_client.Click
 
     End Sub
+
+    Private Sub frmCaja_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DataGridView1.DataSource = Conexiones.Productos("Select * from Clientes")
+
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
 End Class
---
+++ FARMATICS/nodos/Conexiones.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Data.OleDb
+Module Conexiones
+
+    ' Declaracion de variables a usar en el modulo.
+    Public Conexion As OleDbConnection = New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & System.IO.Directory.GetCurrentDirectory() & "\Database.mdb;")
+
+
+
+    Public Sub Abrir()
+        Try
+            Conexion.Open()
+        Catch ex As Exception
+            MsgBox("Error al conectar la base de datos:" & ex.Message)
+        Finally
+            Conexion.Close()
+        End Try
+    End Sub
+
+    Public Function Productos(ByVal texto As String)
+        Conexion.Open()
+
+        Dim datos As New DataSet
+        Dim adaptador As OleDbDataAdapter = New OleDbDataAdapter(texto, Conexion)
+        adaptador.Fill(datos, "Clientes")
+        Return datos
+    End Function
+
+
+
+End Module
