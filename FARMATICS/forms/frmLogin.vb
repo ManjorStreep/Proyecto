f91cdb06d2b2289329d42f8a15748719ebc2453a
Yigs <yigvnsla@gmail.com>
2020-06-21 00:53:03 -0400

    error solucionado frmLogin

--- FARMATICS/forms/frmLogin.vb
+++ FARMATICS/forms/frmLogin.vb
@@ -9,15 +9,8 @@ Public Class frmLogin
         MsgBox("¿Seguro que desea salir de la plataforma?", MsgBoxStyle.OkOnly)
         Me.Close()
     End Sub
-<<<<<<< HEAD
-    Private Sub Button1_Click_2(sender As Object, e As EventArgs)
-        'abrir()
-        x = TextBox1.Text
-        Try
-            'conexion_1.Login(TextBox1.Text, TextBox2.Text)
-=======
+
     Private Sub Button1_Click_2(sender As Object, e As EventArgs) Handles Button1.Click
->>>>>>> 9a0dcd4cb9dccc53897e071bfee4624c67e31704
 
         Dim datos As List(Of String) = Conexiones.ObtenerDatos("Empleados", "WHERE CEDULA ='" & TextBox1.Text & "'")
         If datos.Item(10) = TextBox2.Text Then
