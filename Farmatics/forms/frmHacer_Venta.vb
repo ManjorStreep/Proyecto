16de2b49a2799e5403e696daf8b93c8d68fad90d
ManjorStreep <ManjorStreep@gmail.com>
2020-07-12 18:46:38 -0400

    Terminando casi

--- Farmatics/forms/frmHacer_Venta.vb
+++ Farmatics/forms/frmHacer_Venta.vb
@@ -40,7 +40,8 @@ Public Class frmHacer_Venta
     Private Sub frmHacer_Venta_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: esta línea de código carga datos en la tabla 'DatabaseDataSet.Productos' Puede moverla o quitarla según sea necesario.
         Me.ProductosTableAdapter.Fill(Me.DatabaseDataSet.Productos)
-
+        lb_codigoEmpleado.Text = empleado.Cedula
+        lb_nombreEmpleado.Text = empleado.Nombre
     End Sub
 
     Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
